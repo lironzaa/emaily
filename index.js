8697a73fd49cb2fcd49db676df8090f285736103
lironzaa <lironzaa@gmail>
2019-03-16 23:06:47 +0200

    intial commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' })
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 //נותן להרוקו להחליט לאיזה פורט האפליקציה שלנו צריכה להזמין מתוך קובץ
 //הסביבה שלהם, במידה ואין קובץ האזן ל5000
 app.listen(5000);
